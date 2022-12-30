8fab2072021c958ba6cd6d13f6f4c6aa80e1dcb8
Keiran Morton <kmorton9129@conestogac.on.ca>
2022-12-30 15:08:11 -0500

    Started new version of dice rolling class. This will replace the old one with new and cleaner code.

---
+++ Dice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiwiDiceRoller
+{
+    internal class Dice
+    {
+        const char kAdvantage = 'a';
+        const char kDisadvantage = 'd';
+        const char kNoVantage = 'n';
+
+        private int numOfDice;
+        private int numOfSides;
+        private int modifier;
+        private int difficultyClass;
+        private char advtageState;
+        private bool modEachDie;
+
+
+        Dice(int newNumOfDice, int newNumOfSides, int newModifier = 0, int newDifficultyClass = 0, char newAdvantageState = kNoVantage, bool newModEachDie = false)
+        {
+            numOfDice = newNumOfDice;
+            numOfSides = newNumOfSides;
+            modifier = newModifier;
+            difficultyClass = newDifficultyClass;
+            advtageState = newAdvantageState;
+            modEachDie = newModEachDie;
+        }
+
+
+
+
+    }
+}
